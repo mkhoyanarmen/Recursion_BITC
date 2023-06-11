082e8256e1cfc17098bea0ced4164d11469d7b60
mkhoyanarmen <93216230+mkhoyanarmen@users.noreply.github.com>
2023-06-11 22:14:48 +0400

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion
+{
+    internal class Program
+    {
+        static int fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else
+                return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        static int factorial(int n)
+        {
+            if (n == 1)
+                return n;
+            else
+                return n * factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"fibonacci: {fibonacci(8)}");
+            Console.WriteLine($"factorial: {factorial(4)}");
+        }
+    }
+}
